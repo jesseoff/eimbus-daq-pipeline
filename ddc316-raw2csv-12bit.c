06d7672fbd4dd9d39b1ae1726754470c510bb2f6
Jesse Off <jesseoff@me.com>
2021-02-03 15:59:45 -0500

    Increase robustness of zpub to underrun, add csv converters for ddc316 ADC

---
+++ ddc316-raw2csv-12bit.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+
+/* Takes raw binary FPGA data (32 chans of 12-bit samples) on stdin and 
+ * outputs CSV on stdout. 
+ */
+
+/* ADC sends channel 0 first, channel 31 last */
+static inline void sample(uint32_t v) {
+  int i;
+  static int n = 0;
+  static uint32_t chans[32];
+  chans[n] = v;
+  if (n == 31) {
+    n = 0;
+    for (i = 0; i < 31; i++) printf("%d,",chans[i]);
+    printf("%d\n", chans[31]);
+  } else n++;
+}
+
+/* ADC sends sample bits from from most to least significant */
+static inline void out4(int n) { 
+  static uint32_t x;
+  static int seq = 0;
+  x = (x<<4)|n;
+  if (seq == 2) {
+    sample(x);
+    x = seq = 0;
+  } else seq++;
+}
+
+static inline void out8(int b) {
+  out4(b >> 4);
+  out4(b & 0xf);
+}
+
+int main(int argc, char **argv) {
+  int hw;
+  while ((hw = getchar()) != EOF) { /* FIFO is 16-bit, little-endian */
+    out8(getchar());
+    out8(hw);
+  }
+}
